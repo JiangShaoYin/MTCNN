03456048f09d34bf673c744d8a18cb0519aa4d8a
JiangShaoYin <tooo_cold@163.com>
2019-04-16 14:05:57 +0800

    gen_landmark_24

--- prepare_data/gen_imglist_rnet.py
+++ prepare_data/gen_imglist_rnet.py
@@ -3,10 +3,7 @@ import numpy.random as npr
 import os
 
 data_dir = '.'
-#anno_file = os.path.join(data_dir, "anno.txt")
-
 size = 24
-
 if size == 12:
     net = "PNet"
 elif size == 24:
@@ -16,21 +13,18 @@ elif size == 48:
 
 with open(os.path.join(data_dir, '%s/pos_%s.txt' % (size, size)), 'r') as f:
     pos = f.readlines()
-
 with open(os.path.join(data_dir, '%s/neg_%s.txt' % (size, size)), 'r') as f:
     neg = f.readlines()
-
 with open(os.path.join(data_dir, '%s/part_%s.txt' % (size, size)), 'r') as f:
     part = f.readlines()
-
 with open(os.path.join(data_dir, '%s/landmark_%s_aug.txt' % (size, size)), 'r') as f:
     landmark = f.readlines()
   
 dir_path = os.path.join(data_dir, 'imglists',"RNet")
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
-#write all data
-with open(os.path.join(dir_path, "train_%s_landmark.txt" % (net)), "w") as f:
+
+with open(os.path.join(dir_path, "train_%s_landmark.txt" % (net)), "w") as f:  # write all data
     print(len(neg))
     print(len(pos))
     print(len(part))
--- prepare_data/gen_landmark_aug_24.py
+++ prepare_data/gen_landmark_aug_24.py
@@ -65,13 +65,14 @@ def GenerateData(ftxt, output,net,argument=False):
         F_landmarks = []        
         img = cv2.imread(imgPath)
         assert(img is not None)
-        img_h,img_w,img_c = img.shape
-        gt_box = np.array([bbox.left,bbox.top,bbox.right,bbox.bottom])
-        f_face = img[bbox.top:bbox.bottom+1,bbox.left:bbox.right+1]
-        f_face = cv2.resize(f_face,(size,size))
+        img_h, img_w, img_c = img.shape
+
+        gt_box = np.array([bbox.left, bbox.top, bbox.right, bbox.bottom])
+        f_face_raw = img[bbox.top:bbox.bottom + 1, bbox.left:bbox.right + 1]
+        f_face = cv2.resize(f_face_raw, (size, size))
         landmark = np.zeros((5, 2))
-        #normalize
-        for index, one in enumerate(landmarkGt):
+
+        for index, one in enumerate(landmarkGt):          # 归1化处理
             rv = ((one[0]-gt_box[0])/(gt_box[2]-gt_box[0]), (one[1]-gt_box[1])/(gt_box[3]-gt_box[1]))
             landmark[index] = rv
         
@@ -83,20 +84,18 @@ def GenerateData(ftxt, output,net,argument=False):
             if idx % 100 == 0:
                 print(idx, "images done")
             x1, y1, x2, y2 = gt_box
-            #gt's width
-            gt_w = x2 - x1 + 1
-            #gt's height
-            gt_h = y2 - y1 + 1        
+            gt_w = x2 - x1 + 1     # gt's width
+            gt_h = y2 - y1 + 1     # gt's height
+
             if max(gt_w, gt_h) < 40 or x1 < 0 or y1 < 0:
                 continue
-            #random shift
-            for i in range(10):
+
+            for i in range(10):  # random shift
                 bbox_size = npr.randint(int(min(gt_w, gt_h) * 0.8), np.ceil(1.25 * max(gt_w, gt_h)))
                 delta_x = npr.randint(-gt_w * 0.2, gt_w * 0.2)
                 delta_y = npr.randint(-gt_h * 0.2, gt_h * 0.2)
-                nx1 = max(x1+gt_w/2-bbox_size/2+delta_x,0)
-                ny1 = max(y1+gt_h/2-bbox_size/2+delta_y,0)
-                
+                nx1 = max(x1+gt_w/2-bbox_size/2+delta_x, 0)
+                ny1 = max(y1+gt_h/2-bbox_size/2+delta_y, 0)
                 nx2 = nx1 + bbox_size
                 ny2 = ny1 + bbox_size
                 if nx2 > img_w or ny2 > img_h:
@@ -104,8 +103,8 @@ def GenerateData(ftxt, output,net,argument=False):
                 crop_box = np.array([nx1,ny1,nx2,ny2])
                 cropped_im = img[int(ny1):int(ny2)+1,int(nx1):int(nx2)+1,:]
                 resized_im = cv2.resize(cropped_im, (size, size))
-                #cal iou
-                iou = IoU(crop_box, np.expand_dims(gt_box,0))
+
+                iou = IoU(crop_box, np.expand_dims(gt_box,0))     #cal iou
                 if iou > 0.65:
                     F_imgs.append(resized_im)
                     #normalize
@@ -141,7 +140,7 @@ def GenerateData(ftxt, output,net,argument=False):
                         F_landmarks.append(landmark_flipped.reshape(10))                
                     
                     #inverse clockwise rotation
-                    if random.choice([0,1]) > 0: 
+                    if random.choice([0, 1]) > 0:
                         face_rotated_by_alpha, landmark_rotated = rotate(img, bbox, \
                                                                          bbox.reprojectLandmark(landmark_), -5)#顺时针旋转
                         landmark_rotated = bbox.projectLandmark(landmark_rotated)
@@ -158,10 +157,8 @@ def GenerateData(ftxt, output,net,argument=False):
 
             for i in range(len(F_imgs)):
                 print(image_id)
-
                 if np.sum(np.where(F_landmarks[i] <= 0, 1, 0)) > 0:
                     continue
-
                 if np.sum(np.where(F_landmarks[i] >= 1, 1, 0)) > 0:
                     continue
 
@@ -169,14 +166,8 @@ def GenerateData(ftxt, output,net,argument=False):
                 landmarks = map(str,list(F_landmarks[i]))
                 f.write(join(dstdir,"%d.jpg" %(image_id))+" -2 "+" ".join(landmarks)+"\n")
                 image_id = image_id + 1
-            
-    #print F_imgs.shape
-    #print F_landmarks.shape
-    #F_imgs = processImage(F_imgs)
-    #shuffle_in_unison_scary(F_imgs, F_landmarks)
-    
     f.close()
-    return F_imgs,F_landmarks
+    return F_imgs, F_landmarks
 
 
 if __name__ == '__main__':
